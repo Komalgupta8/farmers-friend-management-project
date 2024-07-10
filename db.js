f6b4ad37ca9dabc511aa9068e42682b80c574598
ishaan khan <ishaank704@gmail.com>
2024-07-10 17:20:35 +0530

    Few Changes

--- db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('Error connecting to MongoDB:', err));
